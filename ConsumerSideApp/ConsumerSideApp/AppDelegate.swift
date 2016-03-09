7bbe0aec4c12fb9e455bdec60d3d1793b43bc5d3
Frederik Lohner <freddylohner@gmail.com>
2016-03-09 12:35:33 -0700

    Base loging screen

--- ConsumerSideApp/ConsumerSideApp/AppDelegate.swift
+++ ConsumerSideApp/ConsumerSideApp/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        func preferredStatusBarStyle() -> UIStatusBarStyle {
+            return UIStatusBarStyle.LightContent
+        }
+        
+        
         // Override point for customization after application launch.
         return true
     }
--- ConsumerSideApp/ConsumerSideApp/LoginScreen.swift
+++ ConsumerSideApp/ConsumerSideApp/LoginScreen.swift
@@ -13,66 +13,117 @@ class LoginScreen: UIViewController {
     private var usernameEntry = UITextField()
     private var passwordEntry = UITextField()
     private var loginButton = UIButton()
-    private var loginLabel = UILabel()
+    private var loginLabelTopBar = UIView()
+    private var loginLabelMidBar = UIView()
+    private var loginLabelBottomBar = UIView()
     private var passwordLabel = UILabel()
+    private let gradientLayer = CAGradientLayer()
+    private let iconImage = UIImageView()
+    private let scrollView = UIScrollView()
+    private let containerView = UIView()
+    
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
     
     override func viewDidLoad() {
-        self.view.addSubview(loginLabel)
-        self.view.addSubview(usernameEntry)
-        self.view.addSubview(passwordLabel)
-        self.view.addSubview(passwordEntry)
-        self.view.addSubview(loginButton)
+        gradientLayer.frame = self.view.bounds
         
-        usernameEntry.borderStyle = .RoundedRect
-        usernameEntry.placeholder = "CU Identikey"
+        let color1 = UIColor(red:0.19, green:0.25, blue:0.35, alpha:1.0).CGColor as CGColorRef
+        let color2 = UIColor(red:0.15, green:0.21, blue:0.29, alpha:1.0).CGColor as CGColorRef
+        gradientLayer.colors = [color1, color2]
+        gradientLayer.locations = [0.0, 1.0]
+        self.view.layer.addSublayer(gradientLayer)
         
-        passwordEntry.borderStyle = .RoundedRect
+        self.view.addSubview(scrollView)
+        scrollView.snp_makeConstraints { (make) -> Void in
+            make.top.right.bottom.left.equalTo(self.view)
+        }
+        scrollView.addSubview(containerView)
+        containerView.snp_makeConstraints { (make) -> Void in
+            make.left.right.top.bottom.equalTo(self.view)
+            make.centerX.equalTo(scrollView)
+        }
+        containerView.addSubview(loginLabelTopBar)
+        containerView.addSubview(loginLabelMidBar)
+        containerView.addSubview(loginLabelBottomBar)
+        containerView.addSubview(usernameEntry)
+        containerView.addSubview(passwordLabel)
+        containerView.addSubview(passwordEntry)
+        containerView.addSubview(loginButton)
+        containerView.addSubview(iconImage)
+        
+        
+        iconImage.image = UIImage(named: "Icon")
+        iconImage.snp_makeConstraints { (make) -> Void in
+            make.height.equalTo(213)
+            make.width.equalTo(300)
+            make.centerX.equalTo(containerView)
+            make.top.equalTo(containerView).offset(100)
+        }
+//        iconImage.hidden = true
+        
+        usernameEntry.attributedPlaceholder  = NSAttributedString(string:"CU Identikey",
+            attributes:[NSForegroundColorAttributeName: UIColor(red:0.69, green:0.73, blue:0.75, alpha:1.0)])
+        usernameEntry.textColor = UIColor(red:0.69, green:0.73, blue:0.75, alpha:1.0)
+        
         passwordEntry.secureTextEntry = true
-        passwordEntry.placeholder = "Identikey password"
+        passwordEntry.attributedPlaceholder  = NSAttributedString(string:"********",
+            attributes:[NSForegroundColorAttributeName: UIColor(red:0.69, green:0.73, blue:0.75, alpha:1.0)])
+        passwordEntry.textColor = UIColor(red:0.69, green:0.73, blue:0.75, alpha:1.0)
         
-        
-        loginLabel.text = "Login"
-        passwordLabel.text = "Password"
-        
-        loginButton.backgroundColor = UIColor.blackColor()
-        
-        loginLabel.snp_makeConstraints { (make) -> Void in
-//            make.bottom.equalTo(usernameEntry.snp_top).offset(-5)
+        loginLabelTopBar.backgroundColor = UIColor(red:0.69, green:0.73, blue:0.75, alpha:1.0)
+        loginLabelTopBar.snp_makeConstraints { (make) -> Void in
             make.centerX.equalTo(usernameEntry)
-            make.centerY.equalTo(self.view).offset(-100)
+            make.centerY.equalTo(containerView).offset(70)
+            make.height.equalTo(1)
+            make.width.equalTo(250)
         }
         
         usernameEntry.snp_makeConstraints { (make) -> Void in
-            make.centerX.equalTo(self.view)
-            make.top.equalTo(loginLabel.snp_bottom).offset(5)
+            make.centerX.equalTo(containerView)
+            make.top.equalTo(loginLabelTopBar.snp_bottom).offset(0)
             make.width.equalTo(200)
             make.height.equalTo(50)
+        }
+        loginLabelMidBar.backgroundColor = UIColor(red:0.69, green:0.73, blue:0.75, alpha:1.0)
+        loginLabelMidBar.snp_makeConstraints { (make) -> Void in
+            make.centerX.equalTo(usernameEntry)
+            make.top.equalTo(usernameEntry.snp_bottom)
+            make.height.equalTo(1)
+            make.width.equalTo(250)
         }
         passwordLabel.snp_makeConstraints { (make) -> Void in
             make.top.equalTo(usernameEntry.snp_bottom).offset(5)
             make.centerX.equalTo(usernameEntry)
         }
         passwordEntry.snp_makeConstraints { (make) -> Void in
-            make.centerX.equalTo(self.view)
+            make.centerX.equalTo(containerView)
             make.top.equalTo(passwordLabel.snp_bottom).offset(5)
             make.width.equalTo(200)
             make.height.equalTo(50)
         }
+        loginLabelBottomBar.backgroundColor = UIColor(red:0.69, green:0.73, blue:0.75, alpha:1.0)
+        loginLabelBottomBar.snp_makeConstraints { (make) -> Void in
+            make.centerX.equalTo(usernameEntry)
+            make.top.equalTo(passwordEntry.snp_bottom)
+            make.height.equalTo(1)
+            make.width.equalTo(250)
+        }
         
+        loginButton.backgroundColor = UIColor(red:0.88, green:0.26, blue:0.26, alpha:1.0)
+        loginButton.backgroundColor = UIColor.whiteColor()
+        loginButton.setTitle("Login", forState: UIControlState.Normal)
+        loginButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
         loginButton.snp_makeConstraints { (make) -> Void in
             make.top.equalTo(passwordEntry.snp_bottom).offset(10)
-            make.centerX.equalTo(self.view)
-            make.height.width.equalTo(50)
+            make.centerX.equalTo(containerView)
+            make.height.equalTo(50)
+            make.left.right.equalTo(loginLabelBottomBar)
         }
-        self.title = "Login"
-        self.navigationController?.navigationBar.backgroundColor = UIColor(red:0.18, green:0.19, blue:0.24, alpha:1.0)
+        self.navigationController?.navigationBar.hidden = true
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
+
 }
 

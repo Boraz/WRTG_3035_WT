066b5b167eb090697617894abb6f9c3f5ae4c501
Anonymous-Stranger <akashgaonkar@gmail.com>
2016-04-18 10:41:06 -0600

    commit for merging

--- js/index.js
+++ js/index.js
@@ -2,53 +2,55 @@
 
 $('select').material_select();
 
+var $submit = $('#submit');
+var $email = $('#email');
+var $name = $('#nickname');
+var $food = $('#food');
 
-var root = new Firebase('https://wt-online.firebaseio.com/');
+var fbroot = new Firebase('https://wt-online.firebaseio.com/');
 
-var taskListRef = root.child('email');
+var foods = fbroot.child('foods');
 
-$('#submit').click(function() {
-  console.log('the submit button is clicked');
-  var email = $('#email').val();
-  var taskObject = {
-    email: email
-  }
-
-  if (email == "") {
-    alert("Invalid entry detected.");
-    console.log("INVALID ENTRY");
-    return;
-  } else {
-    var newTaskRef = taskListRef.push();
-    newTaskRef.set(taskObject);
-  }
-
-  var first_n = $('#first_name').val();
-  var taskObject = {
-    email: first_name
-  }
-
-  if (first_name == "") {
-    alert("Invalid entry detected.");
-    console.log("INVALID ENTRY");
-    return
-  } else {
-    var newTaskRef = taskListRef.push();
-    newTaskRef.set(taskObject);
-  }
-
-  Materialize.toast('Your Order has been recieved!', 4000, 'rounded');
-});
-
-var ref = new Firebase('https://wt-online.firebaseio.com/');
-ref.child('email').orderByValue().on('value', function(snapshot) {
-  
-  var emails = snapshot;
-  document.getElementById("emails").innerHTML = "";
-
-  snapshot.forEach(function(snapshot) {
-    emails = snapshot.val();
-    $('#emails').append('<li>' + emails.email + '</li>');
+foods.once('value', function(snapshot){
+  snapshot.forEach(function(f){
+    f = f.val();
+    $food.append('<option data-icon="images/'+f.image+'">'+f.name+'</option>');
   });
-
+  $food.material_select();
 });
+
+// var taskListRef = root.child('email');
+
+// $submit.click(function() {
+//   console.log('the submit button is clicked');
+//   var email = $email.val();
+
+//   var taskObject = {
+//     email: email
+//   }
+
+//   if (email == '') {
+//     alert('Invalid entry detected.');
+//     console.log('INVALID ENTRY');
+//     return;
+//   } else {
+//     var newTaskRef = taskListRef.push();
+//     newTaskRef.set(taskObject);
+//   }
+
+//   var name = $name.val();
+//   var taskObject = {
+//     email: name
+//   }
+
+//   if (name == '') {
+//     alert('Invalid entry detected.');
+//     console.log('INVALID ENTRY');
+//     return
+//   } else {
+//     var newTaskRef = taskListRef.push();
+//     newTaskRef.set(taskObject);
+//   }
+
+//   Materialize.toast('Your Order has been recieved!', 4000, 'rounded');
+// });
